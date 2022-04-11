f242592c759516c3cbd50d831a677ca3ea9530f4
JBxss <juanseb100@gmail.com>
2022-04-11 14:40:30 -0500

    [ADD] About component with responsive design

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import "./about.css"
+import ME from "../../assets/N2PuvF9Q_400x400.jpg"
+import {FaAward, FaFolderOpen} from "react-icons/fa"
 
 const about = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="Me"/>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>Only Personal Projects</small>
+            </article>
+
+            <article className='about__card'>
+              <FaFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5 Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eos blanditiis quo voluptate, hic aut suscipit dolor? Debitis error ipsum inventore deleniti asperiores ad blanditiis obcaecati quaerat? Quam, explicabo quae?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
