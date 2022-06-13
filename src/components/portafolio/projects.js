52bed5d3c3a92d2d6dd93bc77c078b0cc3db89b6
JBxss <juanseb100@gmail.com>
2022-06-13 15:58:54 -0500

    [UPDT] Portafolio update

--- src/components/portafolio/projects.js
+++ src/components/portafolio/projects.js
@@ -1,6 +1,8 @@
 import IMG1 from "../../assets/portafolio1.jpg";
 import IMG2 from "../../assets/portafolio2.png";
 import IMG3 from "../../assets/portafolio3.png";
+import IMG4 from "../../assets/portafolio4.png";
+import IMG5 from "../../assets/portafolio5.png";
 
 const data = [
   {
@@ -23,6 +25,20 @@ const data = [
     title: "HyperBlog",
     github: "https://github.com/JBxss/HyperBlog",
     demo: "https://jbxss.github.io/HyperBlog/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "ZonaABC",
+    github: "https://github.com/JBxss/ZonaABC",
+    demo: "https://jbxss.github.io/ZonaABC/",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Petgram",
+    github: "https://github.com/JBxss/Petgram",
+    demo: "https://jbxss.github.io/Petgram/",
   },
 ];
 
