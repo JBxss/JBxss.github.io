b4dc79339b675fe1ef1ca7f0bce445c41e66a5f2
JBxss <juanseb100@gmail.com>
2023-08-28 10:53:04 -0500

    [ADD] estructura del home

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div>Social</div>
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/jbxss/" className="home__social-icon" target='_blank'><i class="uil uil-linkedin-alt"></i></a>
+        <a href="https://github.com/JBxss" className="home__social-icon" target='_blank'><i class="uil uil-github-alt"></i></a>
+    </div>
   )
 }
 
