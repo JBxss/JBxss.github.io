00c42e49a870aa7f9cfac77a8d251121e68181fe
JBxss <juanseb100@gmail.com>
2023-08-29 12:43:09 -0500

    [ADD] estructura del services component

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">What i offer</span>
+
+      <div className="services__container container grid">
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">Product <br /> designer</h3>
+          </div>
+
+          <span className="services__button">
+            View More{" "}
+            <i className="uil uil-arrow-right servies__button-icon"></i>
+          </span>
+
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil-times services__modal-close"></i>
+              <h3 className="services__modal-title">Product designer</h3>
+              <p className="services__modal-description">
+                Ability to design all kinds of formats, from landing pages to
+                e-Commerce stores
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">Product <br /> designer</h3>
+          </div>
+
+          <span className="services__button">
+            View More{" "}
+            <i className="uil uil-arrow-right servies__button-icon"></i>
+          </span>
+
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil-times services__modal-close"></i>
+              <h3 className="services__modal-title">Product designer</h3>
+              <p className="services__modal-description">
+                Ability to design all kinds of formats, from landing pages to
+                e-Commerce stores
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">Product <br /> designer</h3>
+          </div>
+
+          <span className="services__button">
+            View More{" "}
+            <i className="uil uil-arrow-right servies__button-icon"></i>
+          </span>
+
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil-times services__modal-close"></i>
+              <h3 className="services__modal-title">Product designer</h3>
+              <p className="services__modal-description">
+                Ability to design all kinds of formats, from landing pages to
+                e-Commerce stores
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop user interface.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Services;
