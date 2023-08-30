dc7846f3ad2a4bd3e56ec5346b856ca780cc80e1
JBxss <juanseb100@gmail.com>
2023-08-30 13:02:04 -0500

    [ADD] estructura del testimonial component

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./qualification.css";
 
 const Qualification = () => {
-  const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState(2);
   const toggleTab = (index) => {
     setToggleState(index);
   };
---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,25 @@
+import Image1 from "../../assets/photo2.jpg"
+
+export const Data = [
+    {
+      id: 1,
+      image: Image1,
+      title: "Jhon Doe",
+      description:
+        "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+      id: 2,
+      image: Image1,
+      title: "Harry Clinton",
+      description:
+        "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+      id: 3,
+      image: Image1,
+      title: "Sara Cill",
+      description:
+        "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+  ];
