f6c8357e35e484a0fc8247d02897153c82324b12
JBxss <juanseb100@gmail.com>
2023-08-31 23:46:32 -0500

    [ADD] estructura portafolio component

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,49 @@
+import Work1 from "../../assets/photo2.jpg";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Web design",
+    category: "web",
+  },
+  {
+    id: 2,
+    image: Work1,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 3,
+    image: Work1,
+    title: "Brand design",
+    category: "design",
+  },
+  {
+    id: 4,
+    image: Work1,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 5,
+    image: Work1,
+    title: "Web design",
+    category: "web",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+  {
+    name: "design",
+  },
+];
