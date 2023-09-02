c67f5920c20a1ca1e3565b0b5babfcdfde3c19b7
Juan Bossa <juanseb100@gmail.com>
2023-09-01 19:57:45 -0500

    [ADD] style skills

--- src/components/skills/Mobile.jsx
+++ src/components/skills/Mobile.jsx
@@ -11,7 +11,7 @@ const Mobile = () => {
           {mobile.map(({ name, image }) => {
             return (
               <div className="skills__data">
-                <i class="bx bx-badge-check"></i>
+                <img className="skills__img" src={image} alt="" />
                 <div>
                   <h3 className="skills__name">{name}</h3>
                 </div>
