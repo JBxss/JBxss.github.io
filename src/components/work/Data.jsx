6fe64ad9ead806e746552a6d52bbe8266c0a64ad
JBxss <juanseb100@gmail.com>
2023-09-03 16:45:11 -0500

    [ADD] more portfolio items

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,3 +1,5 @@
+import Api from "../../assets/portfolio/api.jpeg";
+import IA from "../../assets/portfolio/ia.jpeg";
 import Work0 from "../../assets/portfolio/none.png";
 import Work1 from "../../assets/portfolio/crudphp.png";
 import Work2 from "../../assets/portfolio/pokemonphp.png";
@@ -6,26 +8,20 @@ import Work4 from "../../assets/portfolio/petgram.png";
 import Work7 from "../../assets/portfolio/fastjobs.png";
 import Work8 from "../../assets/portfolio/hyperblog.png";
 import Work12 from "../../assets/portfolio/zonabc.png";
+import Work14 from "../../assets/portfolio/fitnessjs.png";
+import Work15 from "../../assets/portfolio/pluyer.jpg";
 
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
-    title: "C.R.U.D on PHP+MySQL",
-    code: "https://github.com/JBxss/C.R.U.D-on-PHP-MySQL",
-    demo: "",
+    image: Work15,
+    title: "Pluyer Media Player",
+    code: "https://github.com/JBxss/Pluyer-Media-Player",
+    demo: "https://jbxss.github.io/Pluyer-Media-Player/",
     category: "web",
   },
   {
     id: 2,
-    image: Work2,
-    title: "PokédexPHP",
-    code: "https://github.com/JBxss/PokedexPHP",
-    demo: "",
-    category: "web",
-  },
-  {
-    id: 3,
     image: Work3,
     title: "React Shop",
     code: "https://github.com/JBxss/React-Shop",
@@ -33,7 +29,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 4,
+    id: 3,
     image: Work4,
     title: "Petgram",
     code: "https://github.com/JBxss/Petgram",
@@ -41,7 +37,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 5,
+    id: 4,
     image: Work0,
     title: "MenuRestaurantPHP",
     code: "https://github.com/JBxss/MenuRestaurantPHP",
@@ -49,7 +45,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 6,
+    id: 5,
     image: Work0,
     title: "AbstractFactoryPHP",
     code: "https://github.com/JBxss/AbstractFactoryPHP",
@@ -57,7 +53,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 7,
+    id: 6,
     image: Work7,
     title: "FastJobs",
     code: "https://github.com/JBxss/FastJobs",
@@ -65,7 +61,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 8,
+    id: 7,
     image: Work8,
     title: "HyperBlog",
     code: "https://github.com/JBxss/HyperBlog",
@@ -73,7 +69,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 9,
+    id: 8,
     image: Work0,
     title: "Little Notes",
     code: "https://github.com/JBxss/Little-Notes",
@@ -81,7 +77,7 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 10,
+    id: 9,
     image: Work0,
     title: "Health Track",
     code: "https://github.com/JBxss/Health-Track",
@@ -89,12 +85,20 @@ export const projectsData = [
     category: "web",
   },
   {
-    id: 11,
-    image: Work0,
+    id: 10,
+    image: Api,
     title: "API with FlightPHP+JWT",
     code: "https://github.com/JBxss/API-with-FlightPHP-JWT",
     demo: "",
     category: "api",
+  },
+  {
+    id: 11,
+    image: Work2,
+    title: "PokédexPHP",
+    code: "https://github.com/JBxss/PokedexPHP",
+    demo: "",
+    category: "web",
   },
   {
     id: 12,
@@ -103,6 +107,54 @@ export const projectsData = [
     code: "https://github.com/JBxss/ZonaABC",
     demo: "https://jbxss.github.io/ZonaABC/",
     category: "web",
+  },
+  {
+    id: 13,
+    image: Api,
+    title: "API with LeafPHP",
+    code: "https://github.com/JBxss/API-with-LeafPHP",
+    demo: "",
+    category: "api",
+  },
+  {
+    id: 14,
+    image: Work14,
+    title: "FitnessJS",
+    code: "https://github.com/JBxss/FitnessJS",
+    demo: "",
+    category: "web",
+  },
+  {
+    id: 15,
+    image: IA,
+    title: "Data Analysis",
+    code: "https://github.com/JBxss/Data-Analysis",
+    demo: "",
+    category: "ia",
+  },
+  {
+    id: 16,
+    image: Api,
+    title: "API NodeJS+PostgreSQL+JWT",
+    code: "https://github.com/JBxss/API-NodeJS-PostgreSQL-JWT",
+    demo: "",
+    category: "api",
+  },
+  {
+    id: 17,
+    image: Work1,
+    title: "C.R.U.D on PHP+MySQL",
+    code: "https://github.com/JBxss/C.R.U.D-on-PHP-MySQL",
+    demo: "",
+    category: "web",
+  },
+  {
+    id: 18,
+    image: IA,
+    title: "JosePT",
+    code: "https://github.com/JBxss/JosePT",
+    demo: "",
+    category: "ia",
   },
 ];
 
