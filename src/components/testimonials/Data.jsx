bed2acc4014a99e380ac653dcdad41432e33e420
JBxss <juanseb100@gmail.com>
2023-09-06 00:06:48 -0500

    [ADD] testimonial labels

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,25 +1,36 @@
-import Image1 from "../../assets/photo2.jpg"
+import Image1 from "../../assets/testimonial/testimonial1.jpeg"
+import Image2 from "../../assets/testimonial/testimonial2.jpeg"
 
-export const Data = [
+export const DataES = [
     {
       id: 1,
       image: Image1,
-      title: "Jhon Doe",
+      title: "Isai Marmolejo",
       description:
-        "A really good job, all aspects of the project were followed step by step and with good results.",
+        "Servicial cuando uno lo necesita es un buen compañero de equipo trabaja súper bien en equipo, paciente al explicar un tema, aplicado y coordinado en duda trabajos",
     },
     {
       id: 2,
-      image: Image1,
-      title: "Harry Clinton",
+      image: Image2,
+      title: "Edgar Consuegra",
       description:
-        "A really good job, all aspects of the project were followed step by step and with good results.",
+        "Eficiente al momento de realizar trabajos en equipo tomando las mejores decisiones y guiando a los demás integrantes si hace falta.",
+    },
+  ];
+
+  export const DataEN = [
+    {
+      id: 1,
+      image: Image1,
+      title: "Isai Marmolejo",
+      description:
+        "Helpful when you need him, he is a good teammate, works very well in a team, patient when explaining a subject, applied and coordinated when in doubt, works very well.",
     },
     {
-      id: 3,
-      image: Image1,
-      title: "Sara Cill",
+      id: 2,
+      image: Image2,
+      title: "Edgar Consuegra",
       description:
-        "A really good job, all aspects of the project were followed step by step and with good results.",
+        "Efficient in teamwork, making the best decisions and guiding other team members if necessary.",
     },
   ];
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "./testimonials.css";
-import { Data } from "./Data";
+import { DataES, DataEN } from "./Data";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,16 +13,23 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 
 const Testimonials = () => {
+  const { t, i18n } = useTranslation(["testimonials"]);
+  const ReviewStrings = i18n.language === "en" ? DataEN : DataES;
+
   return (
     <section className="testimonial container section">
-      <h2 className="section__title">My clients say</h2>
-      <span className="section__subtitle">Testimonial</span>
+      <span className="section__subtitle">{t("testimonials_subtitle")}</span>
+      <h2 className="section__title">{t("testimonials_title")}</h2>
 
       <Swiper
         className="testimonial__container"
         loop={true}
         grabCursor={true}
         spaceBetween={24}
+        autoplay={{
+          delay: 2250,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
@@ -36,14 +44,17 @@ const Testimonials = () => {
         }}
         modules={[Pagination]}
       >
-        {Data.map(({ id, image, title, description }) => {
+        {ReviewStrings.map(({ id, image, title, description }) => {
           return (
             <SwiperSlide className="testimonial__card" key={id}>
               <img src={image} alt="" className="testimonial__img" />
               <h3 className="testimonial__name">{title}</h3>
-              <p className="testimonial__description">{description}</p>
-              <a href="https://www.linkedin.com/in/jbxss/" className="testimonial__button">
-                Verify{" "}
+              <p className="testimonial__description">"{description}"</p>
+              <a
+                href="https://www.linkedin.com/in/jbxss/"
+                className="testimonial__button"
+              >
+                {t("testimonials_verify")} 
                 <i className="bx bx-right-arrow-alt testimonial__button-icon"></i>
               </a>
             </SwiperSlide>
