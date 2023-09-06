1680481371f4e2858e0a00b28b6504a35cf942a8
JBxss <juanseb100@gmail.com>
2023-09-05 22:05:06 -0500

    [ADD] services labels

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,67 +1,69 @@
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import "./services.css"
 
 const Services = () => {
+  const { t } = useTranslation(["services"]);
     const [toggleState, setToggleState] = useState(0);
     const toggleTab = (index) => {
         setToggleState(index);
     }
   return (
     <section className="services section" id="services">
-      <h2 className="section__title">Services</h2>
-      <span className="section__subtitle">What i offer</span>
+      <span className="section__subtitle">{t("services_subtitle")}</span>
+      <h2 className="section__title">{t("services_title")}</h2>
+      
 
       <div className="services__container container grid">
 
         <div className="services__content">
           <div>
             <i className="uil uil-web-grid services__icon"></i>
-            <h3 className="services__title">Web <br /> Development</h3>
+            <h3 className="services__title">{t("services_modal1-title")} <br /> {t("services_modal1-subtitle")}</h3>
           </div>
 
           <span className="services__button" onClick={() => toggleTab(1)}>
-            View More{" "}
+          {t("services_button")}{" "}
             <i className="uil uil-arrow-right services__button-icon"></i>
           </span>
 
           <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-              <h3 className="services__modal-title">Product designer</h3>
+              <h3 className="services__modal-title">{t("services_modal1-title")} {t("services_modal1-subtitle")}</h3>
               <p className="services__modal-description">
-                Ability to design all kinds of formats, from landing pages to
-                e-Commerce stores
+              {t("services_modal1-description")}
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal1-skill1")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal1-skill2")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal1-skill3")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal1-skill4")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal1-skill5")}
                   </p>
                 </li>
               </ul>
@@ -72,52 +74,51 @@ const Services = () => {
         <div className="services__content">
           <div>
             <i className="uil uil-sitemap services__icon"></i>
-            <h3 className="services__title">Software <br /> Architecture</h3>
+            <h3 className="services__title">{t("services_modal2-title")} <br /> {t("services_modal2-subtitle")}</h3>
           </div>
 
           <span className="services__button" onClick={() => toggleTab(2)}>
-            View More{" "}
+          {t("services_button")}{" "}
             <i className="uil uil-arrow-right services__button-icon"></i>
           </span>
 
           <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-              <h3 className="services__modal-title">Designer</h3>
+              <h3 className="services__modal-title">{t("services_modal2-title")} {t("services_modal2-subtitle")}</h3>
               <p className="services__modal-description">
-                Ability to design all kinds of formats, from landing pages to
-                e-Commerce stores
+              {t("services_modal2-description")}
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal2-skill1")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal2-skill2")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal2-skill3")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal2-skill4")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal2-skill5")}
                   </p>
                 </li>
               </ul>
@@ -128,52 +129,51 @@ const Services = () => {
         <div className="services__content">
           <div>
             <i className="uil uil-wrench services__icon"></i>
-            <h3 className="services__title">Technical <br /> Support</h3>
+            <h3 className="services__title">{t("services_modal3-title")} <br /> {t("services_modal3-subtitle")}</h3>
           </div>
 
           <span className="services__button" onClick={() => toggleTab(3)}>
-            View More{" "}
+          {t("services_button")}{" "}
             <i className="uil uil-arrow-right services__button-icon"></i>
           </span>
 
           <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-              <h3 className="services__modal-title">Productor</h3>
+              <h3 className="services__modal-title">{t("services_modal3-title")} {t("services_modal3-subtitle")}</h3>
               <p className="services__modal-description">
-                Ability to design all kinds of formats, from landing pages to
-                e-Commerce stores
+              {t("services_modal3-description")}
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal3-skill1")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal3-skill2")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal3-skill3")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal3-skill4")}
                   </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop user interface.
+                  {t("services_modal3-skill5")}
                   </p>
                 </li>
               </ul>
